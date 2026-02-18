6c46b3d54d2151f2b9887e7a69ca5dcc439fdda7
Srihari Pasagadugula <sriharipasagadugula@gmail.com>
2026-02-18 16:01:42 +0530

    Clean Vite boilerplate and prepare minimal frontend base

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="p-8 text-xl font-bold text-blue-600">
+      FlowBoard Frontend
+    </div>
   </StrictMode>,
-)
+);
