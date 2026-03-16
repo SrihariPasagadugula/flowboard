fd0c2b76a81fbb6bbb4bdc097fa3495925e778cb
Srihari Pasagadugula <sriharipasagadugula@gmail.com>
2026-03-16 12:28:19 +0530

    feat: implement axios auto logout on 401/403 responses

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { store } from "../app/store";
+import { logout } from "../features/auth/authSlice";
 
 export const api = axios.create({
   baseURL: "http://localhost:8080/api",
@@ -15,3 +16,16 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401 || error.response?.status === 403) {
+      store.dispatch(logout());
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
